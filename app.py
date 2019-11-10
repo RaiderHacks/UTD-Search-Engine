800def3c4e8de19c74c9c9ff5f64e5183613c900
Joseph Martinez <39905973+NotJoeMartinez@users.noreply.github.com>
2019-11-10 01:37:06 -0600

    working text with links

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template
 from scrape import find_links 
+from markupsafe import Markup, escape
+
 
 app = Flask(__name__)
 
@@ -14,7 +16,8 @@ def my_form_post():
     links = find_links(text, target_url)
     # processed_text = text.upper()
     # return processed_text
-    return render_template('results.html', links=links, target_url=target_url, text=text)
+    
+    return render_template('results.html', links=links)
 
 
 @app.route('/results.html')
--- scrape.py
+++ scrape.py
@@ -12,7 +12,8 @@ def find_links(word, target_url):
     req = requests.get(url)
     soup = BeautifulSoup(req.content, 'html.parser')
 
-    links_with_word = []
+
+
     
     # soup.find_all("a", href=re.compile(word))
     # raw_links = soup.find_all("a", href=lambda href: href or word in href)
@@ -21,14 +22,14 @@ def find_links(word, target_url):
         # print(tag.name)
 
     objects_with_key = soup.find_all('a',text=re.compile(word))
+
     
-    for object in objects_with_key:
+    
+    # for object in objects_with_key:
         # if the keyword is in the object append it to links with word
-        links_with_word.append(url + object.get('href') + object.string)
+        # links_with_word.append( url + object.get('href') + object.text)
+    return objects_with_key
 
-
-    print(links_with_word)
-    return links_with_word
 
         
     # return str(links_with_word)
