b932889e23cfa1f8a1d6dbdf3e0f5d88b503f0e0
Tanveer Salim <Tanveer.Salim@ttu.edu>
2019-11-09 19:36:40 -0600

    Added virtualenv and its directory scrapingEnv

---
+++ get_walrus.py
@@ -0,0 +1,47 @@
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+
+def simple_get(url):
+    """
+    Attempts to get the content at 'url' by making an HTTP GET request.
+    If the content-type of response is some kind of HTML/XML, return the
+    text content, otherwise return None.
+    """
+
+    try:
+        with closing(get(url,stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'. format(url,str(e)))
+        return None
+
+def is_good_response(resp):
+    """
+    Returns True if the response seems to be HTML, false otherwise
+    """
+
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200
+        and content_type is not None
+        and content_type.find('html') > -1)
+
+def log_error(e):
+    """
+    It is always a  good idea to log errors.
+    This function just prints them, but you
+    can make it do anything.
+    """
+
+    print(e)
+
+raw_html = simple_get('https://www.cnn.com/specials/politics/president-donald-trump-45')
+# raw_html = open('contrived.html').read()
+html = BeautifulSoup(raw_html,'html.parser')
+for a in html.select('a href'):
+    if a['a href']=='trump':
+        print(a.text)
---
+++ lawson.py
@@ -0,0 +1,5 @@
+from urllib.request import urlopen
+
+html = urlopen('http://pythonscraping.com/pages/page1.html')
+
+print(html.read())
