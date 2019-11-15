197fd3e129ee0f30b7b16a3995c647783d8d0694
Tanveer Salim <Tanveer.Salim@ttu.edu>
2019-11-14 23:02:41 -0600

    Added ip2long to convert ipv4 decimal string to a long int

--- url2ipv4.py
+++ url2ipv4.py
@@ -3,4 +3,15 @@ import socket, struct
 def url2ipv4(hostname):
     return socket.gethostbyname('google.com')
 
-print(url2ipv4('duckduckgo.com'))
+def ip2long(ip):
+    """
+    Convert an IP string to long
+    """
+    packedIP = socket.inet_aton(ip)
+    return struct.unpack("!L",packedIP)[0]
+
+ipv4str = url2ipv4('duckduckgo.com')
+
+ipv4long = ip2long(ipv4str)
+
+print(ipv4long)
